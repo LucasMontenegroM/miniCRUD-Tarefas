860f535ebbce1d143d840c5878911e5b0f2ece35
LucasMontenegroM <lucasmontereira@gmail.com>
2024-08-18 12:44:44 -0300

    Migration Usuario criada

---
+++ src/database/migrations/20240816225227_create_user.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('usuario', (table) => {
+        table.increments('id').primary();
+        table.string('nome').notNullable();
+        table.string('email').notNullable().unique();
+        table.string('senha').notNullable();
+        table.string('CEP', 8).notNullable();
+        table.date('data_nascimento').notNullable();
+});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+};
